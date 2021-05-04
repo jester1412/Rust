67e79374bbb0bed3a0e66c4fd358a89d3617845f
Thammanoon Phattramarut <blackjester1412@gmail.com>
2021-05-04 18:09:15 +0700

    Delete target dir in lifetime example

--- lifetime/target/debug/lifetime.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/thammanoon.ph/GIT/Rust/project/lifetime/target/debug/lifetime: /mnt/c/Users/thammanoon.ph/GIT/Rust/project/lifetime/src/main.rs
--- lifetime/target/rls/debug/deps/lifetime-df3c127e03bebdae.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/thammanoon.ph/GIT/Rust/project/lifetime/target/rls/debug/deps/lifetime-df3c127e03bebdae.rmeta: src/main.rs
-
-/mnt/c/Users/thammanoon.ph/GIT/Rust/project/lifetime/target/rls/debug/deps/lifetime-df3c127e03bebdae.d: src/main.rs
-
-src/main.rs:
