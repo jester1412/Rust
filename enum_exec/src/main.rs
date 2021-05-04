939347ceb6b38d54d7bc8944cbe5071ca5511acf
Thammanoon Phattramarut <blackjester1412@gmail.com>
2021-05-04 19:25:01 +0700

    Add Some Comment

--- enum_exec/src/main.rs
+++ enum_exec/src/main.rs
@@ -14,12 +14,15 @@ enum Coin {
 }
 
 fn main() {
-    //let coin = Coin::Penny;
-    let coin = Coin::Quarter(UsState::Alabama);
+    //let coin = Coin::Penny; // --> set coin to be enum Coin Penny
+    let coin = Coin::Quarter(UsState::Alabama); // --> set coin to be enum Coin Quarter and set enum UsState Alabama
     let mut count = 0;
+    // Matching Flow
+    // If coin is enum Quarter print name of state
     if let Coin::Quarter(state) = coin {
         println!("State quarter from {:?}!", state);
     } else {
+    // if coin is not enum Quarter add 1 to count
         count += 1;
     }
     println!("{}",count);
