14d1d915d24a8201a1864e28baa7d7bc6df0cf38
Thammanoon Phattramarut <blackjester1412@gmail.com>
2021-05-08 03:21:06 +0700

    Refactor Some Functions to use generics type

--- cal_stat/src/main.rs
+++ cal_stat/src/main.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
-// TODO : refactor code to not borrow the v parameter
+use std::hash::Hash;
+// TODO -> DONE : refactor code to not borrow the v parameter
 fn main() {
     // TODO?? : try using array instead of vector ( will be much easier )
     // TODO : find out how can format float without add demicals
-    let v: Vec<f32> = vec![1.0, 2.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0];
+    let v: Vec<i32> = vec![1, 2, 2, 3, 4, 5, 6, 7, 8];
     let mean = find_mean(&v);
     println!("{:.2}", mean);  
     let median = find_median(&v);
@@ -12,17 +13,16 @@ fn main() {
     println!("{}", mode);
 }
 
-fn find_mean(v: &Vec<f32>) -> f32 {
+fn find_mean(v: &Vec<i32>) -> f32 {
     // Explain : 
-    let len = v.len() as f32;
-    let mut val: f32 = 0.0;
-    for i in v.iter() {
-        val += i;
-    }
-    return val/len;
+    v.iter().sum::<i32>() as f32 / v.len() as f32
 }
 
-fn find_median(v: &Vec<f32>) -> f32{
+fn find_median<T>(v: &Vec<T>) -> T
+// Refactor : Change f32 Type to Generics Type
+where
+    T: PartialOrd + Copy
+{
 
     // Explain : Clone v to v_sort becuase when sort_by() the vector is change
     // TODO: need to return the original v ( will solve by passing reference instead of borrow v ?? )
@@ -36,7 +36,10 @@ fn find_median(v: &Vec<f32>) -> f32{
     return v_sort[vlen];
 }
 
-fn find_mode(v: &Vec<f32>) -> f32 {
+fn find_mode<T>(v: &Vec<T>) -> T 
+where
+    T: Hash + Eq + Copy
+{
 
     // Explain : Create new HashMap
     let mut mode = HashMap::new();
@@ -44,7 +47,7 @@ fn find_mode(v: &Vec<f32>) -> f32 {
     //           and if have duplicate Key, Increment Value by one
     for i in v.iter() {        
         //De-reference mode and i because mode and i are reference and can't calculate
-        *mode.entry(*i as i32).or_insert(0) += 1 ;
+        *mode.entry(*i).or_insert(0) += 1 ;
     }
 
     // TODO : find the meaning of this 
@@ -52,12 +55,10 @@ fn find_mode(v: &Vec<f32>) -> f32 {
     //            max_by_key is mapping reference value in results, in this case it's tuple<Key: i32,Value: f32> , to _ and count then return max value
     //            map is self explanatory
     //            expect is for error handling stuff
-    let result = mode.into_iter()
-    .max_by_key(|&(_, count)| count)
-    .map(|(val, _)| val)
+    let result = mode.into_iter() //-> make mode into iter
+    .max_by_key(|&(_, count)| count) // -> |&(_, count)| is Closure that input is reference of tuple<_,count> and output return count, basically find max count
+    .map(|(val, _)| val) // -> Closure Mapping Key
     .expect("Cannot compute the mode of zero numbers");
     
-    return result as f32;
-
-
+    return result;
 }
